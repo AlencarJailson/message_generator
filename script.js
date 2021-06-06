67ec901a1f68e881e84615c6f07d475df1bf4382
AlencarJailson <alencarjailson88@gmail.com>
2021-06-06 17:10:00 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,1 @@
+const messages = ['Keep your friends close', 'Run Forrest', 'I’ll be back', 'Hasta la vista Baby', 'I love the smell of napalm in the morning', 'May the Force be with you', 'My precious'];
