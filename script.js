bde469560cc98e12c925a9638234b60a86acbc7e
AlencarJailson <alencarjailson88@gmail.com>
2021-06-07 07:14:51 -0300

    random movie messages

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-const messages = ['Keep your friends close', 'Run Forrest', 'I’ll be back', 'Hasta la vista Baby', 'I love the smell of napalm in the morning', 'May the Force be with you', 'My precious'];
+const arrayMessages = ['Keep your friends close - The godfather', 'Run Forrest, run - Forest Gump', 'I’ll be back - Teminator', 'Hasta la vista Baby - Teminator 2', 'I love the smell of napalm in the morning - Apocalypse Now', 'May the Force be with you - Star Wars', 'My precious - The Lords of the rings'];
+const generator = () => Math.floor(Math.random() * arrayMessages.length); 
+const generatedMessage = num1 => `${arrayMessages[num1]}`;
+console.log(generatedMessage(generator()));
